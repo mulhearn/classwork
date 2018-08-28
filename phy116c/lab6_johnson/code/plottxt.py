f78d0f3dbb290ce7193b6787fadc49489eb31cfe
Michael Mulhearn <mulhearn@physics.ucdavis.edu>
2018-08-27 17:01:42 -0700

    updates to lab6

--- phy116c/lab6_johnson/code/plottxt.py
+++ phy116c/lab6_johnson/code/plottxt.py
@@ -17,7 +17,7 @@ txtdat = np.loadtxt("raw_waveforms.txt")
 #txtdat = np.loadtxt("one_run.txt")
 
 print txtdat.size
-nruns = txtdat.size / (max_samples*2)
+nruns = txtdat.size // (max_samples*2)
 print "detected data for ", nruns, " runs."
 if (nruns==1):
     txtdat = np.reshape(txtdat,(1,2*max_samples))
@@ -42,7 +42,7 @@ plt.xlim(0,0.5)
 plt.show()
 
 fig, ax = plt.subplots(1,1)
-ax.semilogy(f, psd)
+ax.semilogy(f, pavg)
 ax.set_xlabel('frequency [kHz]')
 ax.set_ylabel('PSD [mV^2/kHz]')
 ax.set_ylim(1E-10, 1E10)
